b7ad19998584703fffa2cb067f35f2a0a4ae88d1
andrew boldrin <andrewboldrin@hotmail.com>
2022-09-26 22:25:54 -0300

    feat: markdown input

--- src/components/core/Container/Container.tsx
+++ src/components/core/Container/Container.tsx
@@ -1,9 +1,14 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
   background-color: ${(props) => props.theme.bg};
   color: ${(props) => props.theme.color};
   font-size: 1.8rem;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 1rem;
+
+  @media (max-width: 700px) {
+    grid-template-columns: 1fr;
+  }
 `;
--- src/pages/Index/InputArea/InputArea.tsx
+++ src/pages/Index/InputArea/InputArea.tsx
@@ -1,14 +1,37 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useContext, useState } from "react";
+import { ThemeContext } from "styled-components";
 
 export const InputArea = (): ReactElement => {
+  const theme = useContext(ThemeContext);
+  const [inputText, setInputText] = useState<string>("");
+  const textByLine = inputText.split("\n");
+
+  console.log(textByLine);
+
+  const handleInputChange = (newValue: string) => {
+    setInputText(newValue);
+  };
+
   return (
     <div
       style={{
-        width: "50vw",
         height: "100vh",
+        padding: "1rem",
       }}
     >
-      inputarea
+      <textarea
+        style={{
+          width: "100%",
+          height: "100%",
+          padding: 0,
+          margin: 0,
+          backgroundColor: theme.bg,
+          color: theme.color,
+          fontSize: "1rem",
+        }}
+        defaultValue={inputText}
+        onChange={(e) => handleInputChange(e.currentTarget.value)}
+      ></textarea>
     </div>
   );
 };
