f6b3a4eb861af5f1a592005a4df97aa7a72eafa7
andrew boldrin <andrewboldrin@hotmail.com>
2023-01-11 16:48:20 -0300

    refactor: refact inputArea

--- src/pages/Index/InputArea/InputArea.tsx
+++ src/pages/Index/InputArea/InputArea.tsx
@@ -1,8 +1,27 @@
-import React, { ReactElement, useContext, useState } from "react";
-import { ThemeContext } from "styled-components";
+import { ReactElement, useState } from "react";
+import styled from "styled-components";
+
+const StyledTextArea = styled.textarea`
+  width: 100%;
+  height: 100%;
+  padding: 0;
+  margin: 0;
+  background-color: ${(props) => props.theme.bg};
+  color: ${(props) => props.theme.color};
+  fontsize: 1rem;
+  border: none;
+  border-right: 1px solid ${(props) => props.theme.color};
+  @media (max-width: 700px) {
+    border-right: none;
+  }
+`;
+
+const StyledInputContainer = styled.div`
+  height: 100vh;
+  padding: 1rem;
+`;
 
 export const InputArea = ({ changeInputLine }: any): ReactElement => {
-  const theme = useContext(ThemeContext);
   const [inputText, setInputText] = useState<string>("");
 
   const handleInputChange = (newValue: string) => {
@@ -12,28 +31,12 @@ export const InputArea = ({ changeInputLine }: any): ReactElement => {
   };
 
   return (
-    <div
-      style={{
-        height: "100vh",
-        padding: "1rem",
-      }}
-    >
-      <textarea
-        style={{
-          width: "100%",
-          height: "100%",
-          padding: 0,
-          margin: 0,
-          backgroundColor: theme.bg,
-          color: theme.color,
-          fontSize: "1rem",
-          border: "none",
-          borderRight: `1px solid ${theme.color}`,
-        }}
+    <StyledInputContainer>
+      <StyledTextArea
         placeholder={`Insert your text here!\n\n Follow the examples:\n\n # title\n\n ## subtitle\n\n ### medium text\n\n #### small text\n\n * Marked Text\n\n - List item 1\n\n - List item 2\n\n > Highlight Important Text\n\n >> Highlight Text\n\nYou can see this clicking in them help button.`}
         defaultValue={inputText}
         onChange={(e) => handleInputChange(e.currentTarget.value)}
-      ></textarea>
-    </div>
+      ></StyledTextArea>
+    </StyledInputContainer>
   );
 };
